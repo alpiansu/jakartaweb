94cbcbebbc3bff0b3d65b40299cd2221e956a62a
alpiansu <alpiansusanto.idm@gmail.com>
2024-07-26 15:57:20 +0700

    fix update home feature

--- resources/views/fe/home/features.blade.php
+++ resources/views/fe/home/features.blade.php
@@ -3,9 +3,9 @@
         <div class="row">
             <div class="col-12 text-center" data-aos="fade-down" data-aos-delay="50">
                 <div class="section-title">
-                    <h1 class="display-4 fw-semibold">Features</h1>
+                    <h1 class="display-4 fw-semibold">{{ $feature_text->title }}</h1>
                     <div class="line"></div>
-                    <p>We love to craft digital experiences for brands rather than crap and more lorem ipsums and do crazy skills</p>
+                    <p>{{ $feature_text->content }}</p>
                 </div>
             </div>
         </div>
