ce214d8b4f32a4935302471efa6e62ee0fb1818f
alpiansu <alpiansusanto.idm@gmail.com>
2024-07-26 15:50:36 +0700

    dynamic page fe insight

--- app/Http/Controllers/FE/InsightController.php
+++ app/Http/Controllers/FE/InsightController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\FE;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Blog;
 
 class InsightController extends Controller
 {
     public function index()
     {
-        return view("fe.insight.index");
+        $blogs = Blog::all();
+        return view("fe.insight.index", compact("blogs"));
     }
 }
--- resources/views/fe/insight/index.blade.php
+++ resources/views/fe/insight/index.blade.php
@@ -1,94 +1,36 @@
-@extends('fe/master-fe')
+@extends('fe.master-fe')
 @section('title', 'Jakartaweb')
 
 @section('content')
-  <section id="blog" class="mt-5 p-5">
+<section id="blog" class="mt-5 p-5">
     <div class="container">
-      <div class="row">
-        <div class="col-12 text-center" data-aos="fade-down" data-aos-delay="150">
-          <div class="section-title">
-            <h1 class="display-4 fw-semibold">Insights</h1>
-            <div class="line"></div>
-            <p>
-              Our Insight page presents in-depth analysis of key trends and strategic opportunities that can help organizations achieve competitive advantage in a dynamic market landscape. Through comprehensive research and data review, we identify critical insights that can drive innovation, improve efficiency, and foster sustainable growth. The insights shared aim to empower business leaders to make smarter decisions, optimize their strategies, and build lasting competitive edge. By providing valuable strategic foresight, our Insight page aspires to be a trusted reference for navigating the challenges and possibilities of the future.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md-4 pt-4" data-aos="fade-down" data-aos-delay="150">
-          <div class="blog-post image-zoom">
-            <div class="image-zoom-wrapper">
-              <img src="../../public/assets/img/blog-post-1.jpg" alt="blog1" />
+        <div class="row">
+            <div class="col-12 text-center" data-aos="fade-down" data-aos-delay="150">
+                <div class="section-title">
+                    <h1 class="display-4 fw-semibold">Insights</h1>
+                    <div class="line"></div>
+                    <p>
+                        Our Insight page presents in-depth analysis of key trends and strategic opportunities that can help organizations achieve competitive advantage in a dynamic market landscape. Through comprehensive research and data review, we identify critical insights that can drive innovation, improve efficiency, and foster sustainable growth. The insights shared aim to empower business leaders to make smarter decisions, optimize their strategies, and build lasting competitive edge. By providing valuable strategic foresight, our Insight page aspires to be a trusted reference for navigating the challenges and possibilities of the future.
+                    </p>
+                </div>
             </div>
-            <h5 class="mt-4">Web Design in 2024</h5>
-            <p class="mb-0">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio animi libero velit.
-            </p>
-            <a href="#" class="mt-0">Read More</a>
-          </div>
         </div>
-        <div class="col-md-4 pt-4" data-aos="fade-down" data-aos-delay="250">
-          <div class="blog-post image-zoom">
-            <div class="image-zoom-wrapper">
-              <img src="../../public/assets/img/blog-post-2.jpg" alt="blog2" />
-            </div>
-            <h5 class="mt-4">Web Design in 2024</h5>
-            <p class="mb-0">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio animi libero velit.
-            </p>
-            <a href="#" class="mt-0">Read More</a>
-          </div>
+        <div class="row">
+            @foreach($blogs as $blog)
+                <div class="col-md-4 pt-4" data-aos="fade-down" data-aos-delay="150">
+                    <div class="blog-post image-zoom">
+                        <div class="image-zoom-wrapper">
+                            <img src="{{ asset('assets/img/' . $blog->image_path) }}" alt="{{ $blog->title }}" />
+                        </div>
+                        <h5 class="mt-4">{{ $blog->title }}</h5>
+                        <p class="mb-0">
+                            {{ Str::limit($blog->description, 100, '...') }}
+                        </p>
+                        <a href="{{ $blog->link }}" class="mt-0">Read More</a>
+                    </div>
+                </div>
+            @endforeach
         </div>
-        <div class="col-md-4 pt-4" data-aos="fade-down" data-aos-delay="350">
-          <div class="blog-post image-zoom">
-            <div class="image-zoom-wrapper">
-              <img src="../../public/assets/img/blog-post-3.jpg" alt="blog3" />
-            </div>
-            <h5 class="mt-4">Web Design in 2024</h5>
-            <p class="mb-0">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio animi libero velit.
-            </p>
-            <a href="#" class="mt-0">Read More</a>
-          </div>
-        </div>
-        <div class="col-md-4 pt-4" data-aos="fade-down" data-aos-delay="150">
-          <div class="blog-post image-zoom">
-            <div class="image-zoom-wrapper">
-              <img src="../../public/assets/img/blog-post-1.jpg" alt="blog1" />
-            </div>
-            <h5 class="mt-4">Web Design in 2024</h5>
-            <p class="mb-0">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio animi libero velit.
-            </p>
-            <a href="#" class="mt-0">Read More</a>
-          </div>
-        </div>
-        <div class="col-md-4 pt-4" data-aos="fade-down" data-aos-delay="250">
-          <div class="blog-post image-zoom">
-            <div class="image-zoom-wrapper">
-              <img src="../../public/assets/img/blog-post-2.jpg" alt="blog2" />
-            </div>
-            <h5 class="mt-4">Web Design in 2024</h5>
-            <p class="mb-0">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio animi libero velit.
-            </p>
-            <a href="#" class="mt-0">Read More</a>
-          </div>
-        </div>
-        <div class="col-md-4 pt-4" data-aos="fade-down" data-aos-delay="350">
-          <div class="blog-post image-zoom">
-            <div class="image-zoom-wrapper">
-              <img src="../../public/assets/img/blog-post-3.jpg" alt="blog3" />
-            </div>
-            <h5 class="mt-4">Web Design in 2024</h5>
-            <p class="mb-0">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio animi libero velit.
-            </p>
-            <a href="#" class="mt-0">Read More</a>
-          </div>
-        </div>
-      </div>
     </div>
-  </section>
-@endsection('content')
+</section>
+@endsection
