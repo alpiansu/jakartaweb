bd8e1ac05c355e5eb1917b094003f1ace439b7fd
alpiansu <alpiansusanto.idm@gmail.com>
2024-08-01 17:07:26 +0700

    update page service

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,8 @@ use App\Http\Controllers\FE\HomeController;
 use App\Http\Controllers\FE\ContactController;
 use App\Http\Controllers\FE\InsightController;
 use App\Http\Controllers\FE\WorkController;
+use App\Http\Controllers\FE\ServiceController;
+use App\Http\Controllers\FE\Service2Controller;
 
 use App\Http\Controllers\Admin\AdminController;
 
@@ -20,6 +22,8 @@ Route::get("/", [HomeController::class, "index"])->name('fe.home');
 Route::get("/contact", [ContactController::class, "index"])->name('fe.contact');
 Route::get("/insight", [InsightController::class, "index"])->name('fe.insight');
 Route::get("/work", [WorkController::class, "index"])->name('fe.work');
+Route::get("/service", [ServiceController::class, "index"])->name('fe.service');
+Route::get("/service2", [Service2Controller::class, "index"])->name('fe.service2');
 
 Auth::routes();
 
